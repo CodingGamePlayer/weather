f0c1f9f38ed5f825aefc7041a1a9bbfa420e84f3
KJS <otw1917@naver.com>
2023-05-10 06:39:58 +0900

    finished configuration of jpa

--- src/main/java/com/weather/repository/JdbcMemoRepository.java
+++ src/main/java/com/weather/repository/JdbcMemoRepository.java
@@ -1,2 +1,43 @@
-package com.weather.repository;public class JdbcMemoRepository {
+package com.weather.repository;
+
+import com.weather.domain.Memo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcMemoRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public Memo save(Memo memo) {
+        String sql = "INSERT INTO memo VALUES (?,?)";
+        jdbcTemplate.update(sql, memo.getId(), memo.getText());
+        return memo;
+    }
+
+    public List<Memo> findAll(){
+        String sql = "select * from memo";
+        return jdbcTemplate.query(sql, memoRowMapper());
+    }
+
+    public Memo findById(int id) {
+        String sql= "select * from memo where id = ?";
+
+        return jdbcTemplate.query(sql, memoRowMapper(), id)
+                .stream().findAny()
+                .orElseThrow(() -> new IllegalArgumentException());
+    }
+
+    private RowMapper<Memo> memoRowMapper() {
+        return ((rs, rowNum) -> new Memo(
+                rs.getInt("id"),
+                rs.getString("text")
+        ));
+    }
+
 }
--- src/test/java/com/weather/repository/JdbcMemoRepositoryTest.java
+++ src/test/java/com/weather/repository/JdbcMemoRepositoryTest.java
@@ -1,4 +1,73 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.weather.repository;
+
+import com.weather.domain.Memo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Transactional
 class JdbcMemoRepositoryTest {
-  
+
+    @Autowired
+    JdbcMemoRepository jdbcMemoRepository;
+
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        //given
+        Memo memo = new Memo(1, "this is memo");
+        //when
+        jdbcMemoRepository.save(memo);
+        Memo findMemo = jdbcMemoRepository.findById(1);
+        //then
+        assertEquals(memo.getId(), findMemo.getId());
+        assertEquals(memo.getText(), findMemo.getText());
+    }
+
+    @Test
+    void findAllMemoTest() {
+        //given
+        List<Memo> memoList = jdbcMemoRepository.findAll();
+        //when
+        assertNotNull(memoList);
+        //then
+    }
+
+    @Test
+    void insert () {
+        //given
+        Memo memo = new Memo();
+        memo.setText("this is memo");
+
+        //when
+        jpaMemoRepository.save(memo);
+
+        //then
+        List<Memo> memos = jpaMemoRepository.findAll();
+        assertEquals(memos.get(0).getId(), memo.getId());
+        assertEquals(memos.get(0).getText(), memo.getText());
+    }
+
+    @Test
+    void findById () {
+        //given
+        Memo memo = new Memo();
+        memo.setText("this is memo");
+
+        //when
+        Memo saved = jpaMemoRepository.save(memo);
+        Memo foundMemo = jpaMemoRepository.findById(saved.getId()).orElseThrow();
+
+        //then
+        assertEquals(memo.getText(), foundMemo.getText());
+    }
 }
